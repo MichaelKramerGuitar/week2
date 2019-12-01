903551dabd88534dc0a23828b4ba7ebf02ad395c
MichaelKramerGuitar <58241823+MichaelKramerGuitar@users.noreply.github.com>
2019-12-01 10:48:17 -0500

    Update Functionsvid.c

--- Functionsvid.c
+++ Functionsvid.c
@@ -3,7 +3,17 @@
 #include <stdio>
 
 //decare 2 functions
-int add_twp_ints(int a, int b);
+int add_two_ints(int a, int b);
 
 int main(void)
 {
+  //as the user for input
+  printf("Give me an integer: ");
+  int x = GetInt();
+  printf("Give me another integer: ")
+  int y = GetInt();
+  
+  //add the two numbers together via a funtion cell
+  int z ("The sum of %i and %i is %i!\n", x, y, z);
+ 
+}
